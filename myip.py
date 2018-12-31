3a0fd4d7514bd412132b1246b4f764caf3859e0f
Gregor Löffler <gregor.loeffler@zoho.com>
2018-12-31 08:44:04 +0100

    Code optimization

--- myip.py
+++ myip.py
@@ -4,67 +4,46 @@ import json
 import sys
 from requests import get
 
-# get ip from API
+
+# get IP from API
 def getip():
     data = get("https://api.myip.com").text
     data = json.loads(data)
     return data
 
-# commandlien argument parser
-def parse_args():
-    parser = argparse.ArgumentParser(description="get public IP information")
-    parser.add_argument("-i",
-                        "--ip",
-                        action="store_true",
-                        help="print only IP")
-    
-    parser.add_argument("-c",
-                        "--country",
-                        action="store_true",
-                        help="print only country")
-    
-    parser.add_argument("-C",
-                        "--country-code",
-                        action="store_true",
-                        help="print only country code")
-    
-    parser.add_argument("-r",
-                        "--raw",
-                        action="store_false",
-                        help="print raw output")
-    return parser.parse_args()
 
-
-args = parse_args()
+# commandline argument parser
+parser = argparse.ArgumentParser(description="get public IP information")
+parser.add_argument("-i", "--ip", action="store_true",
+                    help="print only IP")
+parser.add_argument("-c", "--country",
+                    action="store_true", help="print only country")
+parser.add_argument("-C", "--country-code",
+                    action="store_true", help="print only country code")
+parser.add_argument("-r", "--raw", action="store_false",
+                    help="print raw output")
+args = parser.parse_args()
 output = getip()
 
+# some variables
+ip = args.ip
+country = args.country
+cc = args.country_code
+raw = args.raw
+
 # if nothing is specified, print all
-if not args.ip and not args.country and not args.country_code:
-    if args.raw:
+if not ip and not country and not cc:
+    ip, country, cc = True, True, True
+
+if ip:
+    if raw:
         print("IP: ", end="")
-
     print(output["ip"])
-
-    if args.raw:
+if country:
+    if raw:
         print("Country: ", end="")
-
     print(output["country"])
-
-    if args.raw:
+if cc:
+    if raw:
         print("CC: ", end="")
-
     print(output["cc"])
-else:
-    # print output
-    if args.ip:
-        if args.raw:
-            print("IP: ", end="")
-        print(output["ip"])
-    if args.country:
-        if args.raw:
-            print("Country: ", end="")
-        print(output["country"])
-    if args.country_code:
-        if args.raw:
-            print("CC: ", end="")
-        print(output["cc"])
